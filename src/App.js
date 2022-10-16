e34a315124e1ef0b9ef62a5c5759fc0c961541fa
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2022-10-16 11:27:53 +0200

    refactor: embed BusinessContextsTab component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import BusinessContextsTab from "./components/BusinessContextsTab";
 import TasksList from "./components/TasksList";
 
 function App() {
   return (
     <div className="App">
       <TasksList />
+      <BusinessContextsTab />
     </div>
   );
 }
